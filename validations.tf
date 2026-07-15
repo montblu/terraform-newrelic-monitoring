52bec16688549e507a4a0232d87639710f58830c
Tiago Carrondo <tiago@montblu.eu>
2026-07-15 11:44:37 +0100

    refactor: use terraform_data instead of null_resource for key validation
    terraform_data is a built-in (Terraform >= 1.4) and performs the same
    precondition check, removing the dependency on the hashicorp/null provider.

--- validations.tf
+++ validations.tf
@@ -13,7 +13,7 @@ locals {
 
 # Since all monitor maps are merged, all keys between different types of monitors need to be unique to avoid 
 # overwriting values.
-resource "null_resource" "check_unique_monitor_keys" {
+resource "terraform_data" "check_unique_monitor_keys" {
   lifecycle {
     precondition {
       condition     = length(local.all_monitor_keys) == length(local.all_distinct_monitor_keys)
